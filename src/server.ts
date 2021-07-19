a62b417d9915b4a57891fa088aca5aefea700919
Pedro Dalla Bernardina <l.pedro.nascimento@gmail.com>
2021-07-19 09:32:27 -0300

    Add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUi from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerConfig from "../swagger.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.listen(3333, () => console.log("Server is running!"));
